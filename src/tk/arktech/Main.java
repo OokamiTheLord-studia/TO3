b409822b6c932c48a02f104da574b070d3e35b7c
Jakub Markowski <kuba.xx5@outlook.com>
2018-11-18 23:30:22 +0100

    Vector2D działa

--- src/tk/arktech/Main.java
+++ src/tk/arktech/Main.java
@@ -11,7 +11,10 @@ public class Main {
 
     public static void main(String[] args) {
 
-        
+        Vector2D v1 = new Vector2D(10,10);
+        Vector2D v2 = new Vector2D(5,5);
+
+        System.out.print(v1.toString() + v2.toString() + Double.toString(v1.dot(v2)));
 
 
     }

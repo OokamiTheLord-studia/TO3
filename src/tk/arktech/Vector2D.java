763d1c386d046cb3138ad5cf72c443054d957dc0
Jakub Markowski <kuba.xx5@outlook.com>
2018-11-18 23:31:08 +0100

    Vector2D działa

---
+++ src/tk/arktech/Vector2D.java
@@ -0,0 +1,37 @@
+package tk.arktech;
+
+public class Vector2D {
+    private double x;
+    private double y;
+
+    public Vector2D(double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public double getX() {
+        return x;
+    }
+
+    public void setX(double x) {
+        this.x = x;
+    }
+
+    public double getY() {
+        return y;
+    }
+
+    public void setY(double y) {
+        this.y = y;
+    }
+
+    @Override
+    public String toString() {
+        return "["+x+"]\n["+y+"]\n";
+    }
+
+    public double dot(Vector2D v)
+    {
+        return this.x*v.x+this.y*v.y;
+    }
+}

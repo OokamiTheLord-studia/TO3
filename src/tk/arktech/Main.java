76c00f377c6b9d31b4aacd43104bae6ce5708214
Jakub Markowski <kuba.xx5@outlook.com>
2018-11-18 23:01:47 +0100

    Initial commit

---
+++ src/tk/arktech/Main.java
@@ -0,0 +1,18 @@
+package tk.arktech;
+
+public class Main {
+
+    /*Pobieranie każdej współrzędnej
+    iloczyn skalarny,
+    iloczyn wektorowy,
+    dekorator,
+    adapter*/
+
+
+    public static void main(String[] args) {
+
+        
+
+
+    }
+}
